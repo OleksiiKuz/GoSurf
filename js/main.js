e59f31e962f9ab7437ffd5bd8bcd10c6d8c13d38
OleksiiKuz <61288803+OleksiiKuz@users.noreply.github.com>
2020-06-08 14:00:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -144,6 +144,14 @@ $(function(){
 
    });
 
+   $('a[href*="#"]').on('click', function (e) {
+      e.preventDefault();
+
+      $('html, body').animate({
+         scrollTop: $($(this).attr('href')).offset().top
+      }, 1000, 'linear');
+   });
+
    new WOW().init();
 
 });
